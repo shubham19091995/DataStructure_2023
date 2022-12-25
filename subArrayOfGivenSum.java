9fcd780dc0ca09cac5d79634591de52bea4c68ea
shubham19091995 <106596578+shubham19091995@users.noreply.github.com>
2022-12-25 14:40:20 +0530

    Update subArrayOfGivenSum.java

--- subArrayOfGivenSum.java
+++ subArrayOfGivenSum.java
@@ -2,9 +2,11 @@ import java.util.HashMap;
 
 class hello {
     public static void main(String[] args) {
-        int[] a = { 10, 15, -5, 15, -10, 5 };
+ int[] a = { 10, 15, -5, 15, -10, 5 };
 
         int sum = 5;
+        int start=0;
+        int end=-1;
 
         HashMap<Integer, Integer> values = new HashMap<>();
         int currentSum=0;
@@ -12,17 +14,27 @@ class hello {
 
      for(int i=0;i<a.length;i++){
         currentSum+=a[i];
-        values.put(currentSum, i);
+       
 
         if(currentSum-sum==0){
-            System.out.println("Got It ");
+            start=0 ;end=i;
             break;
         }
 
         if(values.containsKey(currentSum-sum)){
-            System.out.println("Got it");
+            start=values.get(currentSum-sum)+1;
+            end=i;
+            break;
         }
+        values.put(currentSum, i);
 
+     }
+
+     if(end==-1){
+        System.out.println("Not Found");
+     }
+     else{
+        System.out.println(start+"   "+end);
      }
 
     }
